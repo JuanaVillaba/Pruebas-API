da8f92fb98182498c71b1d2d088c267e1118c106
JuanaVillalba <juananicolvillalba@gmail.com>
2026-03-17 12:35:23 -0300

    Index base terminada

--- js/index.js
+++ js/index.js
@@ -1,23 +1,18 @@
-
-function api(){
+function apiPersonajes(){
     fetch("https://rickandmortyapi.com/api/character",{method: "GET"})
     .then((response)=>response.json())
     .then((data)=> {
         renderCharacter(data.results)
-        //console.log(data.results)
-            //for(let index=0; index < data.results.length ;index++){
-            //    console.log(data.results[index].name);
-            //    document.querySelector(".items").innerHTML +=`<li>${data.results[index].name}</li>`
-            //}
+
             }
         )
     .catch((error)=>console.log(error));
 }
 function renderCharacter(arr){
-    for(let index=0; index < arr.length ;index++){
+    for(let index=0; index < 5 ;index++){
                 console.log(arr[index].name);
                 document.querySelector(".items").innerHTML +=`
-                <li>${arr[index].name}</li>
+                <h2>${arr[index].name}</h2>
                 <div class="card" style="width: 18rem;">
                     <img src="${arr[index].image}" class="card-img-top" alt="...">
                     <div class="card-body">
@@ -29,4 +24,58 @@ function renderCharacter(arr){
                 `
             }
 }
-api();
+function apiLocaciones(){
+    fetch("https://rickandmortyapi.com/api/location",{method: "GET"})
+    .then((response)=>response.json())
+    .then((data)=> {
+        locacion(data.results)
+        
+            }
+        )
+    .catch((error)=>console.log(error));
+}
+function locacion(arr){
+        for(let index=0; index < 5 ;index++){
+                console.log(arr[index].name);
+                document.querySelector(".locaciones").innerHTML +=`
+                <h2>${arr[index].name}</h2>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                    <h5 class="card-id"> ID: ${arr[index].id}</h5>
+                    <h5 class="card-dimension"> Dimension: ${arr[index].dimension}</h5>
+                    <h5 class="card-type"> TIPO: ${arr[index].type}</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                `
+        }
+}
+function apiEpisodios(){
+    fetch("https://rickandmortyapi.com/api/episode",{method: "GET"})
+    .then((response)=>response.json())
+    .then((data)=> {
+        episodios(data.results)
+        
+            }
+        )
+    .catch((error)=>console.log(error));
+}
+function episodios(arr){
+    for(let index=0; index < 5 ;index++){
+                console.log(arr[index].name);
+                document.querySelector(".episodes").innerHTML +=`
+                <h2>${arr[index].name}</h2>
+                <div class="card" style="width: 18rem;">
+                    <div class="card-body">
+                    <h5 class="card-air-date"> Fecha de transmicion: ${arr[index].air_date}</h5>
+                    <h5 class="card-created">Fecha de creacion: ${arr[index].created}</h5>
+                    <a href="#" class="btn btn-primary">Go somewhere</a>
+                    </div>
+                </div>
+                `
+            }
+}
+
+apiPersonajes();
+apiLocaciones();
+apiEpisodios();
