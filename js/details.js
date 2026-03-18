f57b35613306feb8507ea129853442828700f11b
maitt-a <maiterocioalvarenga@gmail.com>
2026-03-18 17:55:29 -0300

    Details added

---
+++ js/details.js
@@ -0,0 +1,20 @@
+const URL = "https://rickandmortyapi.com/api/character/1"; // cambiar ID cambiar personaje
+
+fetch(URL)
+  .then(response => response.json())
+  .then(data => {
+    // Imagen
+    document.getElementById("character-img").src = data.image;
+
+    // Info principal
+    document.getElementById("name").textContent = data.name;
+
+    // Datos abajo
+    document.getElementById("name-data").textContent = data.name;
+    document.getElementById("status").textContent = data.status;
+    document.getElementById("species").textContent = data.species;
+    document.getElementById("gender").textContent = data.gender;
+    document.getElementById("origin").textContent = data.origin.name;
+    document.getElementById("location").textContent = data.location.name;
+  })
+  .catch(error => console.log("Error:", error));
